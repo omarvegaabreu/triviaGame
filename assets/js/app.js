abad541eb39f1d84afeb74c477c9013b01f77ca1
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-07-13 01:03:14 -0400

    game complete logic and styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 //Initial values
 
-let counter = 30;
+let counter = 0;
 let currentQuestion = 0;
 let score = 0;
 let lost = 0;
@@ -9,15 +9,48 @@ let timer;
 //function calls
 loadQuestion();
 
-//Display questions and choices together in the browser
+function nextQuestion() {
+  const isQuestionOver = quizQuestions.length - 1 === currentQuestion;
+  if (isQuestionOver) {
+    console.log("game is over!");
+    //display results to html
+    displayResult();
+  } else {
+    currentQuestion++;
+    loadQuestion();
+  }
+}
 
+function timeUp() {
+  clearInterval(timer);
+  lost++;
+  preloadImage("lost");
+  setTimeout(nextQuestion, 2 * 1000);
+}
+
+//30 second timer to start before the function
+function countDown() {
+  counter--;
+
+  $("#time").html(`Timer: ${counter}`);
+
+  if (counter === 0) {
+    timeUp();
+  }
+}
+
+//Display questions and choices together in the browser
 function loadQuestion() {
+  counter = 5;
+  timer = setInterval(countDown, 1000);
+
   const question = quizQuestions[currentQuestion].questions;
   const choices = quizQuestions[currentQuestion].choices;
 
   $("#time").html(`Timer: ${counter}`);
   $("#game").html(`<h4>${question}</h4>
-  ${loadChoices(choices)}`); //loading choices to the questions
+  ${loadChoices(choices)}
+  ${loadRemainingQuestion()}`);
 }
 
 function loadChoices(choices) {
@@ -29,3 +62,81 @@ function loadChoices(choices) {
 
   return result;
 }
+//
+
+$(document).on("click", ".choice", function() {
+  clearInterval(timer);
+  const selectAnswer = $(this).attr("data-answer");
+  const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+
+  if (correctAnswer === selectAnswer) {
+    score++;
+    console.log("wins");
+    preloadImage("win");
+    setTimeout(nextQuestion, 2 * 1000);
+  } else {
+    lost++;
+    console.log("lost");
+    preloadImage("lost");
+    setTimeout(nextQuestion, 2 * 1000);
+  }
+
+  console.log("yeah", selectAnswer);
+});
+
+function displayResult() {
+  const result = `
+        <p>You get ${score} questions's right.</p>
+        <p>You missed ${lost} question.</p>
+        <p>Total questions ${quizQuestions.length} questions's right.</p>
+        <button class="btn btn-primary" id="reset">Reset Game</button>
+    `;
+
+  $("#game").html(result);
+}
+
+$(document).on("click", "#reset", function() {
+  counter = 0;
+  currentQuestion = 0;
+  score = 0;
+  lost = 0;
+  timer = null;
+
+  loadQuestion();
+});
+
+function loadRemainingQuestion() {
+  const remainingQuestion = quizQuestions.length - (currentQuestion + 1);
+  const totalQuestion = quizQuestions.length;
+
+  return `Remaining Question: ${remainingQuestion}/${totalQuestion} `;
+}
+function randomImage(images) {
+  const random = Math.floor(Math.random() * images.length);
+  const randomImage = images[random];
+  return randomImage;
+}
+
+function preloadImage(status) {
+  const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+
+  if (status === "win") {
+    $("#game").html(`
+            <p class="preload-image">Congratulations, you pick the corrrect answer</p>
+            <p class="preload-image">The correct answer is <b>${correctAnswer}</b></p>
+            <img src="${randomImage(winImages)}" />
+        `);
+  } else {
+    $("#game").html(`
+            <p class="preload-image">The correct answer was <b>${correctAnswer}</b></p>
+            <p class="preload-image">You lost pretty bad</p>
+            <img src="${randomImage(looseImages)}" />
+        `);
+  }
+}
+
+$("#start").click(function() {
+  $("#start").remove();
+  $("#time").html(counter);
+  loadQuestion();
+});
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,18 +1,46 @@
 const quizQuestions = [
   {
-    questions: "What is my full name?",
-    choices: ["Omar Vega", "Omar", "Vega", "Jorge Masdival"],
-    correctAnswer: "Omar Vega"
+    questions: "Who was the first female UFC fighter signed?",
+    choices: ["Holly Holm", "Nina Ansaroff", "Ronda Rousey", "Angela Hill"],
+    correctAnswer: "Ronda Rousey"
   },
 
   {
-    questions: "What is my favorite Sport?",
-    choices: ["Soccer", "Baseball", "Football", "Jorge Masdival"],
-    correctAnswer: "Soccer"
+    questions:
+      "Where was the first UFC held out of the continental United States?",
+    choices: ["Japan", "China", "Puerto Rico", "Russia"],
+    correctAnswer: "Puerto Rico"
   },
   {
-    questions: "Where do you live?",
-    choices: ["Santo Domingo", "Miami", "Burgertown", "Jorge Masdival"],
-    correctAnswer: "Miami"
+    questions: "Where is the UFC headquarters located as of 2014?",
+    choices: [
+      "New Orleans,LA",
+      "Los Angeles,CA",
+      "Las Vegas,NV",
+      "Long Island,NY"
+    ],
+    correctAnswer: "Las Vegas,NV"
+  },
+  {
+    questions: "What is the shape of the cage in the UFC?",
+    choices: ["Triangle", "Star", "Circle", "Octagon"],
+    correctAnswer: "Octagon"
+  },
+  {
+    questions: "What year was the UFC founded?",
+    choices: ["1990", "2003", "1993", "1989"],
+    correctAnswer: "1993"
   }
+];
+
+const winImages = [
+  "../assets/images/win1.webp",
+  "../assets/images/win2.webp",
+  "../assets/images/win3.webp"
+];
+
+const looseImages = [
+  "../assets/images/loose1.webp",
+  "../assets/images/loose2.webp",
+  "../assets/images/loose3.gif"
 ];
