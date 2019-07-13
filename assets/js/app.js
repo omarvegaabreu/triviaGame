b354d8dc3f323ada5df06fd6327b25900ab7fb17
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-07-12 21:35:19 -0400

    getting questions from app js and adding to html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,31 @@
-console.log($);
+//Initial values
+
+let counter = 30;
+let currentQuestion = 0;
+let score = 0;
+let lost = 0;
+let timer;
+
+//function calls
+loadQuestion();
+
+//Display questions and choices together in the browser
+
+function loadQuestion() {
+  const question = quizQuestions[currentQuestion].questions;
+  const choices = quizQuestions[currentQuestion].choices;
+
+  $("#time").html(`Timer: ${counter}`);
+  $("#game").html(`<h4>${question}</h4>
+  ${loadChoices(choices)}`); //loading choices to the questions
+}
+
+function loadChoices(choices) {
+  let result = "";
+
+  for (let i = 0; i < choices.length; i++) {
+    result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`;
+  }
+
+  return result;
+}
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,18 @@
-prompt("questions");
+const quizQuestions = [
+  {
+    questions: "What is my full name?",
+    choices: ["Omar Vega", "Omar", "Vega", "Jorge Masdival"],
+    correctAnswer: "Omar Vega"
+  },
+
+  {
+    questions: "What is my favorite Sport?",
+    choices: ["Soccer", "Baseball", "Football", "Jorge Masdival"],
+    correctAnswer: "Soccer"
+  },
+  {
+    questions: "Where do you live?",
+    choices: ["Santo Domingo", "Miami", "Burgertown", "Jorge Masdival"],
+    correctAnswer: "Miami"
+  }
+];
